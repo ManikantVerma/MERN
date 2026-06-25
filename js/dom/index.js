d7b3d239b6ee4b8a932ab90a3ebad3d010a86ef9
Manikant Verma <manikantverma29@gmail.com>
2026-06-25 10:55:43 +0530

    change js

---
+++ js/dom/index.js
@@ -0,0 +1,8 @@
+function change() {
+    let ele = document.getElementsByTagName("h1")
+    ele[0].innerText = "Hello India"
+
+    let ele1 = document.getElementById("param")
+    ele1.innerText = "This is new param"
+    ele1.style.backgroundColor="green"
+}
